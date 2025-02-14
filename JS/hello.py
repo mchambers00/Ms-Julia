7ad0d7c4dbb047afc956ef83aa74b7a7c1e6ad2a
MikeC <michaelchambers001@umb.edu>
2025-02-14 10:39:20 -0500

    xd

---
+++ JS/hello.py
@@ -0,0 +1,24 @@
+
+## print("##########") # 10 Hashtags
+
+ha = "#"
+line = ""
+height = 5
+row = 0 
+while row < height:
+    for i in range(10):
+        line += ha
+    row += 1
+    line += "\n"
+  ###  if i == 9:
+print("\n" + line + "\n")
+    
+    
+## print("#")
+
+# 5 tall 10 wide
+
+# make a visualization. data managemment system
+# make visualy engaging games at least
+# c# is very important to learn
+# michaelv0
